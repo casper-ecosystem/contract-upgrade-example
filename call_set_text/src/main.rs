a866105c329443483d085f91ffd771f2b327d09a
filip@casperlabs.io <filip@casperlabs.io>
2021-01-12 12:01:38 +0000

    cargo fmt file added

--- call_set_text/src/main.rs
+++ call_set_text/src/main.rs
@@ -5,18 +5,19 @@
 #![no_main]
 // This contract will call another contract and execute the "set_text" function in the external contract
 
-use casperlabs_contract::{
-    contract_api::runtime,
-};
-use casperlabs_types::{runtime_args, RuntimeArgs, 
-    ContractPackageHash
-};
-
+use casperlabs_contract::contract_api::runtime;
+use casperlabs_types::{runtime_args, ContractPackageHash, RuntimeArgs};
 
 #[no_mangle]
 pub extern "C" fn call() {
-// We ask for the contract package hash of the external contract that we want to call
-    let contract_package: ContractPackageHash = runtime::get_named_arg("contract_package");
-// We call the external contract and execute the set_text function
-    runtime::call_versioned_contract(contract_package, None, "set_text", runtime_args! {})
+    // We ask for the contract package hash of the external contract that we want to call
+    let contract_package: ContractPackageHash =
+        runtime::get_named_arg("contract_package");
+    // We call the external contract and execute the set_text function
+    runtime::call_versioned_contract(
+        contract_package,
+        None,
+        "set_text",
+        runtime_args! {},
+    )
 }
--- contract/src/main.rs
+++ contract/src/main.rs
@@ -11,10 +11,13 @@ use casperlabs_contract::{
     contract_api::{runtime, storage},
     unwrap_or_revert::UnwrapOrRevert,
 };
-use casperlabs_types::{CLType,runtime_args, RuntimeArgs, CLTyped, CLValue,
-    bytesrepr::{FromBytes, ToBytes}, URef,
-    contracts::{EntryPoints, EntryPoint, NamedKeys, EntryPointAccess, EntryPointType},
-    ContractPackageHash
+use casperlabs_types::{
+    bytesrepr::{FromBytes, ToBytes},
+    contracts::{
+        EntryPoint, EntryPointAccess, EntryPointType, EntryPoints, NamedKeys,
+    },
+    runtime_args, CLType, CLTyped, CLValue, ContractPackageHash, RuntimeArgs,
+    URef,
 };
 
 const METHOD_SET_TEXT: &str = "set_text";
@@ -29,7 +32,6 @@ const CONTRACT_HASH: &str = "text_contract_hash";
 const TEXT_KEY: &str = "text";
 const TEXT_VALUE_V1: &str = "value_one";
 
-
 #[no_mangle]
 pub extern "C" fn set_text() {
     set_key(TEXT_KEY, TEXT_VALUE_V1)
@@ -37,24 +39,34 @@ pub extern "C" fn set_text() {
 
 #[no_mangle]
 pub extern "C" fn upgrade_to() {
-    let installer_package: ContractPackageHash = runtime::get_named_arg("installer_package");
+    let installer_package: ContractPackageHash =
+        runtime::get_named_arg("installer_package");
     let contract_package: ContractPackageHash = get_key(CONTRACT_PACKAGE);
-    
-    runtime::call_versioned_contract(installer_package, None, "install", runtime_args! {
-        "contract_package" => contract_package,
-    })
+
+    runtime::call_versioned_contract(
+        installer_package,
+        None,
+        "install",
+        runtime_args! {
+            "contract_package" => contract_package,
+        },
+    )
 }
 
 #[no_mangle]
 pub extern "C" fn get_access_token() {
-    let access_token: URef = runtime::get_key(ACCESS_TOKEN).unwrap_or_revert().into_uref().unwrap_or_revert();
+    let access_token: URef = runtime::get_key(ACCESS_TOKEN)
+        .unwrap_or_revert()
+        .into_uref()
+        .unwrap_or_revert();
     runtime::ret(CLValue::from_t(access_token).unwrap());
 }
 
 #[no_mangle]
 pub extern "C" fn call() {
-    let (contract_package, access_token) = storage::create_contract_package_at_hash();
-    
+    let (contract_package, access_token) =
+        storage::create_contract_package_at_hash();
+
     let entry_points = {
         let mut entry_points = EntryPoints::new();
         let deposit = EntryPoint::new(
@@ -86,15 +98,20 @@ pub extern "C" fn call() {
 
         entry_points
     };
-    
+
     // this should overwrite the previous contract obj with the new contract obj at the same uref
     let mut named_keys = NamedKeys::new();
     named_keys.insert(ACCESS_TOKEN.to_string(), access_token.into());
-    named_keys.insert(CONTRACT_PACKAGE.to_string(), storage::new_uref(contract_package).into());
-    let (new_contract_hash, _) =
-        storage::add_contract_version(contract_package, entry_points, named_keys);
-    
-    
+    named_keys.insert(
+        CONTRACT_PACKAGE.to_string(),
+        storage::new_uref(contract_package).into(),
+    );
+    let (new_contract_hash, _) = storage::add_contract_version(
+        contract_package,
+        entry_points,
+        named_keys,
+    );
+
     runtime::put_key(CONTRACT_NAME, new_contract_hash.into());
     set_key(CONTRACT_PACKAGE, contract_package); // stores contract package hash under account's named key
     set_key(CONTRACT_HASH, new_contract_hash);
